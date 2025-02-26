aa20e962f4573666a845557aab40d5ac5cb5e482
Pawel Nackiewicz <it.pnackiewicz@gmail.com>
2025-02-26 08:43:55 +0100

    add github action;

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const environment = import.meta.env.VITE_APP_ENV || "unknown";
 
   return (
     <>
@@ -28,8 +29,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p>Env: {environment}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
